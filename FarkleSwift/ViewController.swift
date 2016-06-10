1fd8756d39330bb985f1b26856b806e63963a985
Caleb Talbot <mailcaleb@gmail.com>
2016-06-09 22:09:00 -0500

    Initial Commit

---
+++ FarkleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FarkleSwift
+//
+//  Created by Caleb Talbot on 6/9/16.
+//  Copyright © 2016 Caleb Talbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
